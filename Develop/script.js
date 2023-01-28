53b98b219610ee1cef40a809ae374440686f2380
Carissella <carissebarrswe@gmail.com>
2023-01-28 12:07:49 -0500

    added jsDay and appended current date to the application

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+let saveButton = $('.saveBtn')
+let today = dayjs();
+$('#currentDay').text(today.format('dddd, MMMM D YYYY'));
+
+
 $(function () {
+  saveButton.on('click', )
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
